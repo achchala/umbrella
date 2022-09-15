91c81a1d8b7869524d99268c3bae9f2b7fee8938
Achchala Deepan <achchala2003@gmail.com>
2022-09-15 01:25:05 -0400

    troubleshooting

--- script.js
+++ script.js
@@ -17,20 +17,20 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
-        document.querySelector(".weather").classList.remove("loading");
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
         //removes loading if it's there
     },
     search: function () {
-        this.fetchWeather(document.querySelector(".searchBar").value);
+        this.fetchWeather(document.querySelector(".search-bar").value);
     },
 };
     
@@ -39,8 +39,8 @@ document.querySelector(".search button").addEventListener("click", function () {
     //when the search button is clicked, it gets the content of the search bar and searches for it
 });
 
-document.querySelector(".searchBar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter"){
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
       weather.search();
     }
 });
